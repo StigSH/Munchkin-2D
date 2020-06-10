4cd91b78baaecea4f058e4d4b81c67502eeb9894
StigSH <stig0509@hotmail.com>
2020-06-10 16:45:55 +0200

    Initial commit

---
+++ Assets/Scripts/CardZoom.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class CardZoom : MonoBehaviour
+    {
+        public GameObject Canvas;
+        
+        private GameObject zoomCard;
+        private GameObject startParent;
+
+        public void Awake()
+        {
+            Canvas = GameObject.Find("Main Canvas");
+
+        }
+
+        public void OnHoverEnter()
+        {
+
+            zoomCard = Instantiate(gameObject, new Vector2(Input.mousePosition.x, Input.mousePosition.y + 250),Quaternion.identity);
+            zoomCard.transform.SetParent(Canvas.transform,true);
+
+
+            zoomCard.transform.localPosition = new Vector3(0, 0, 0);
+            RectTransform rect = zoomCard.GetComponent<RectTransform>();
+            rect.localScale = new Vector2(1, 1);
+
+        }
+
+        public void OnHoverExit()
+        {
+            Destroy(zoomCard);
+        }
+
+    }
+}
---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class DragDrop : MonoBehaviour
+    {
+        public GameObject Canvas;
+
+        private bool isDragging = false;
+        private bool isOverDropZone = false;
+        private GameObject playerArea;
+        private GameObject startParent;
+        private Vector2 startPosition;
+
+
+        private void Awake()
+        {
+            Canvas = GameObject.Find("Main Canvas");
+        }
+
+        void Update()   
+        {
+            if(isDragging)
+            {
+                transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+                transform.SetParent(Canvas.transform,true);
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            isOverDropZone = true;
+            playerArea = collision.gameObject;
+        }
+
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            isOverDropZone = false;
+            playerArea = null;
+        }
+
+        public void StartDrag()
+        {
+            startParent = transform.parent.gameObject;
+            startPosition = transform.position;
+            isDragging = true;
+
+        }
+
+        public void EndDrag()
+        {
+            isDragging = false;
+            if(isOverDropZone)
+            {
+                transform.SetParent(playerArea.transform, false);
+            }
+            else
+            {
+                transform.position = startPosition;
+                transform.SetParent(startParent.transform, false);
+            }
+        }
+    }
+}
